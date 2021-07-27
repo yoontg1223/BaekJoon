f03e10dcc83ba1e4173df9aebc3881f7371d0271
윤태건 <yoontg1223@naver.com>
2021-07-27 23:14:44 +0900

    백준 10718번

---
+++ src/algorithm/Problem10718.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+/**
+ * 백준 10718번
+ */
+public class Problem10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
+/*
+class Main {
+    public static void main(String[] args) {
+        System.out.print(("강한친구 대한육군\n").repeat(2));
+    }
+}
+ */
--- src/algorithm/Problem2557.java
+++ src/algorithm/Problem2557.java
@@ -1,5 +1,8 @@
 package algorithm;
 
+/**
+ * 백준 2557번
+ */
 public class Problem2557 {
     public static void main(String[] args){
         System.out.println("Hello World!");
