4fdb524412ce323d3dd69645a7a6d6ceda3adc13
윤태건 <yoontg1223@naver.com>
2021-07-27 23:29:21 +0900

    백준 10172번

---
+++ src/algorithm/Problem10172.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+/**
+ * 백준 10172번
+ */
+public class Problem10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
