f55e2d3767ee16614c94bef04784af56e8620220
윤태건 <yoontg1223@naver.com>
2021-07-30 22:21:29 +0900

    백준 1001번

--- src/algorithm/Problem1000.java
+++ src/algorithm/Problem1000.java
@@ -6,16 +6,8 @@ import java.util.Scanner;
  * 백준 1000번
  */
 public class Problem1000 {
-
-    public static int test1(int A,int B){
-        int C = A + B;
-        return C;
-    }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        int A = s.nextInt();
-        int B = s.nextInt();
-        int D = test1(A,B);
-        System.out.println(D);
+        System.out.println(s.nextInt() + s.nextInt());
     }
 }
---
+++ src/algorithm/Problem1001.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+import java.util.Scanner;
+
+/**
+ * 백준 1001번
+ */
+public class Problem1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A - B);
+    }
+}
