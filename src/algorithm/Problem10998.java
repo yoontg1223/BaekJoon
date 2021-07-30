dc596428508248a9650fe79fcbd08c0a79aa11dd
윤태건 <yoontg1223@naver.com>
2021-07-30 22:37:29 +0900

    백준 10998번

---
+++ src/algorithm/Problem10998.java
@@ -0,0 +1,10 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Problem10998 {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.print(a.nextInt() * a.nextInt());
+    }
+}
