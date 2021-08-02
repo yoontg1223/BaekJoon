2309b9d8d72c8b743887c17306729dae4d87d94d
윤태건 <yoontg1223@naver.com>
2021-08-02 22:30:43 +0900

    백준 2753번

---
+++ src/algorithm/Problem2753.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Problem2753 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int A = s.nextInt();
+        if(A % 100 != 0) {
+            if(A % 4 == 0) {
+                System.out.println("1");
+            }
+            else {
+                System.out.println("0");
+            }
+        }
+        else if (A % 400 == 0) {
+            System.out.println("1");
+        }
+        else {
+            System.out.println("0");
+        }
+    }
+}
